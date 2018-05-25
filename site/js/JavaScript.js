59b3828d48b1d0a4caaca6d1c301aed644eaf2bf
itggot-christoffer-polndorfer-oresten <christoffer.polndorferoresten@itggot.se>
2018-05-25 12:54:33 +0200

    hemsidan är i princip klar nu. mindre grejer att fixa så som bildbehandling

--- site/js/JavaScript.js
+++ site/js/JavaScript.js
@@ -11,8 +11,3 @@ function togglehamburgerMenu(){
     var element = document.querySelector(".hamburger_menu_wrapper");
     element.classList.toggle("show_hamburgermenu");
 }
-
-// function togglehamburgerMenu_header(){
-//     var element = document.querySelector(".nav_bar_header");
-//     element.classList.toggle("black");
-// }
